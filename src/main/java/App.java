880df971c72e797344ecd4958d4a9cb8db91fa11
Jagadeesh <jagadeeshwarrao.k@gmail.com>
2023-09-25 21:58:34 +0530

    updated pom file

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,17 +1,19 @@
 import com.google.common.base.Splitter;
 
-import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
+
 public class App {
-    public  static  final  String EQ=" eq ";
-    public static void main(String[] args) throws UnsupportedEncodingException {
+
+    public static final String EQ = " eq ";
+
+    public static void main(String[] args) {
 
         String filter = "batchCode%20eq%20RTY45%26payGroup%20eq%2055P";
-        String decodeFilter = URLDecoder.decode(filter, StandardCharsets.UTF_8.name());
-        System.out.println("decodeFilter :" + decodeFilter);
+        String decodeFilter = URLDecoder.decode(filter, StandardCharsets.UTF_8);
+        System.out.println("decodeFilter : " + decodeFilter);
 
         Map<String, String> map = Splitter.on("&").withKeyValueSeparator(EQ).split(decodeFilter);
         System.out.println(map.get("batchCode"));
